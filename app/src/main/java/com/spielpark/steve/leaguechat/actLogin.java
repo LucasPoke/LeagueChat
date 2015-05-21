2b58d8e5261372d01430e89599aa31976cadceae
shadowwolf0189 <steve.vernyi@gmail.com>
2015-05-21 10:04:06 -0400

    removed logging

--- app/src/main/java/com/spielpark/steve/leaguechat/actLogin.java
+++ app/src/main/java/com/spielpark/steve/leaguechat/actLogin.java
@@ -1,20 +1,15 @@
 package com.spielpark.steve.leaguechat;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,31 +17,18 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import com.github.theholywaffle.lolchatapi.ChatServer;
-import com.spielpark.steve.leaguechat.news.NewsReader;
 import com.spielpark.steve.leaguechat.news.NewsTask;
-import com.spielpark.steve.leaguechat.service.ChatService;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class actLogin extends Activity {
+    @SuppressWarnings("FieldCanBeLocal")
     private final int NUM_ARTICLES = 8;
-    private final ArrayList<String> regions = new ArrayList();
+    private final ArrayList<String> regions = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,11 +49,11 @@ public class actLogin extends Activity {
         char[] password = ((EditText) findViewById(R.id.et_enter_password)).getText().toString().toCharArray();
         ((EditText)findViewById(R.id.et_enter_password)).setText("");
         Intent intent = new Intent(this, actLogin_transition.class);
-        intent.putExtra("un", NotMyPasswords.notMyUserName);
-        intent.putExtra("pw", NotMyPasswords.notMyPassword.toCharArray());
+        //intent.putExtra("un", NotMyPasswords.notMyUserName);
+        //intent.putExtra("pw", NotMyPasswords.notMyPassword.toCharArray());
         intent.putExtra("region", ((Spinner)findViewById(R.id.region_select)).getSelectedItem().toString());
-        //intent.putExtra("un", user);
-        //intent.putExtra("pw", password);
+        intent.putExtra("un", user);
+        intent.putExtra("pw", password);
         startActivity(intent);
     }
 
@@ -103,8 +85,7 @@ public class actLogin extends Activity {
                 ((TextView)view.findViewById(R.id.lstTextView)).setMovementMethod(LinkMovementMethod.getInstance());
                 ((TextView)view.findViewById(R.id.lstTextView)).setLinksClickable(true);
                 ((TextView)view.findViewById(R.id.lstTextView)).setAutoLinkMask(Linkify.WEB_URLS);
-                ((TextView)view.findViewById(R.id.lstTextView)).performClick();
-                System.out.println(((TextView) view.findViewById(R.id.lstTextView)).getText());
+                view.findViewById(R.id.lstTextView).performClick();
             }
         });
         AsyncTask getTopPosts = new NewsTask(txtPosts, (RelativeLayout) findViewById(R.id.loadingPanel), REDDIT_POSTS, NUM_ARTICLES).execute();
