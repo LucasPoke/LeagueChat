0b6b25cfdf5cd1fe630ae270e17399f9bd6876c6
shadowwolf0189 <steve.vernyi@gmail.com>
2015-05-21 10:04:51 -0400

    timestamps are here! daily, akin to Steam. normal timestamps coming soon

--- app/src/main/java/com/spielpark/steve/leaguechat/chatpage/ChatAdapter.java
+++ app/src/main/java/com/spielpark/steve/leaguechat/chatpage/ChatAdapter.java
@@ -7,12 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
-import android.widget.ResourceCursorAdapter;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.spielpark.steve.leaguechat.R;
 import com.spielpark.steve.leaguechat.service.ChatService;
+import com.spielpark.steve.leaguechat.util.Util;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by Steve on 2/6/2015.
@@ -31,16 +33,37 @@ public class ChatAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         String from = cursor.getString(cursor.getColumnIndex(MessageDB.TableEntry.COLUMN_FROM));
         String message = cursor.getString(cursor.getColumnIndex(MessageDB.TableEntry.COLUMN_MESSAGE));
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(MessageDB.TableEntry.COLUMN_TIME)));
+        int curDay = cal.get(Calendar.DAY_OF_YEAR);
+        TextView datetext = ((TextView) view.findViewById(R.id.date));
+        if (cursor.getPosition() == 0 || diffDays(cursor)) {
+            datetext.setVisibility(View.VISIBLE);
+            datetext.setText(Util.getTimeChatFormat(cal));
+            Log.d("CA/BV", "New Date View: " + Util.getTimeChatFormat(cal));
+        } else {
+            datetext.setVisibility(View.GONE);
+        }
         TextView chat;
         if (from.equalsIgnoreCase(ChatService.getUserName())) {
             chat = (TextView) view.findViewById(R.id.rightChat);
-            view.findViewById(R.id.leftChat).setVisibility(view.GONE);
+            view.findViewById(R.id.leftChat).setVisibility(View.GONE);
         } else {
             chat = (TextView) view.findViewById(R.id.leftChat);
-            view.findViewById(R.id.rightChat).setVisibility(view.GONE);
+            view.findViewById(R.id.rightChat).setVisibility(View.GONE);
         }
-        chat.setVisibility(view.VISIBLE);
+        chat.setVisibility(View.VISIBLE);
         chat.setText(message);
+    }
+
+    private boolean diffDays(Cursor cursor) {
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(MessageDB.TableEntry.COLUMN_TIME)));
+        int day1 = cal.get(Calendar.DAY_OF_YEAR);
+        cursor.moveToPrevious();
+        cal.setTimeInMillis(cursor.getLong(cursor.getColumnIndex(MessageDB.TableEntry.COLUMN_TIME)));
+        int day2 = cal.get(Calendar.DAY_OF_YEAR);
+        return day2 != day1;
     }
 
     @Override
