658c8de65a6e3d025ab7d3e3a6a27f3331e7d316
shadowwolf0189 <steve.vernyi@gmail.com>
2015-06-01 17:30:49 -0400

    un/pw

--- app/src/main/java/com/spielpark/steve/leaguechat/actLogin.java
+++ app/src/main/java/com/spielpark/steve/leaguechat/actLogin.java
@@ -61,11 +61,11 @@ public class actLogin extends Activity {
         char[] password = ((EditText) findViewById(R.id.et_enter_password)).getText().toString().toCharArray();
         ((EditText)findViewById(R.id.et_enter_password)).setText("");
         Intent intent = new Intent(this, actLogin_transition.class);
-        intent.putExtra("un", NotMyPasswords.notMyUserName);
-        intent.putExtra("pw", NotMyPasswords.notMyPassword.toCharArray());
+        //intent.putExtra("un", NotMyPasswords.notMyUserName);
+        //intent.putExtra("pw", NotMyPasswords.notMyPassword.toCharArray());
         intent.putExtra("region", ((Spinner)findViewById(R.id.region_select)).getSelectedItem().toString());
-        //intent.putExtra("un", user);
-        //intent.putExtra("pw", password);
+        intent.putExtra("un", user);
+        intent.putExtra("pw", password);
         startActivity(intent);
     }
 
