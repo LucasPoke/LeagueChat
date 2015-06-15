6d608cdd4a1b345dc2cdcf5869b0c2cebae38a85
shadowwolf0189 <steve.vernyi@gmail.com>
2015-06-15 16:09:23 -0400

    nullchecking

--- app/src/main/java/com/spielpark/steve/leaguechat/usersettings/Settings.java
+++ app/src/main/java/com/spielpark/steve/leaguechat/usersettings/Settings.java
@@ -31,6 +31,10 @@ public class Settings {
     }
 
 
+    public static boolean isInit() {
+        return preferences != null;
+    }
+
     public static int getUserPic() {
         return preferences.getInt(USER_PIC, 8);
     }
